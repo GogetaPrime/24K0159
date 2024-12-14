d0902ac3008f272341a3727b573c493ce271e173
GogetaPrime <159478771+GogetaPrime@users.noreply.github.com>
2024-12-14 13:12:43 +0300

    Add files via upload

--- Case10.jl
+++ Case10.jl
@@ -1,20 +1,10 @@
 using HorizonSideRobots
-rotate(side::HorizonSide)=HorizonSide((Int(side)+1)%4)
-inverse(side::HorizonSide)=HorizonSide((Int(side)+2)%4)
-function HorizonSideRobots.move!(robot,side,num_steps)
-    for _ in 1:num_steps
-        move!(robot,side)
-    end
-end
-function distance!(robot,side)
-    counter=0
-    while (!isborder(robot,side))
-        move!(robot,side)
-        counter+=1
-    end
-    move!(robot,inverse(side),counter)
-    return counter
-end
+
+inverse(side::HorizonSide)=HorizonSide(mod((Int(side)+2),4))
+
+HorizonSideRobots.move!(robot,side,num_steps)=for _ in 1:num_steps move!(robot,side) end
+
+
 function do_upora!(robot,side)
     counter=0
     while (!isborder(robot,side))
@@ -23,59 +13,52 @@ function do_upora!(robot,side)
     end
     return counter
 end
-function drawline!(robot,side,size)
-    for _ in 1:size
-        putmarker!(robot)
-        if (!isborder(robot,side)) #Граничный случай для правых квадартов
-            move!(robot,side)
-        else
-            size-=1
-        end
-    end
-    move!(robot,inverse(side),size)
+mutable struct CoordRobot
+    robot::Robot
+    x::Int
+    y::Int
 end
-function drawsquare!(robot,size)
-    for _ in 1:size
-        drawline!(robot,Ost,size) 
-        if  (!isborder(robot,Nord)) #Граничный случай для верхних квадратов
-            move!(robot,Nord)
-        else
-            size-=1
-        end
-    end
-    move!(robot,Sud,size)
-end
-function drawsquaredline!(robot,side,n)
+HorizonSideRobots.isborder(robot::CoordRobot,side)=isborder(robot.robot,side)
+
+HorizonSideRobots.putmarker!(robot::CoordRobot)=putmarker!(robot.robot)
+
+function HorizonSideRobots.move!(robot::CoordRobot,side::HorizonSide)
     
-    while (distance!(robot,side)>=(n-1))
-        drawsquare!(robot,n)
-        moveneed!(robot,side,2*n)
+    if (side==Nord)
+        robot.y+=1
+    elseif (side==Sud)
+        robot.y-=1
+    elseif (side==Ost)
+        robot.x+=1
+    else
+        robot.x-=1
+    end
+    move!(robot.robot,side)
+end
+function passline!(robot::CoordRobot,side,N)
+    while (!isborder(robot,side))
+        (((robot.x%(2*N))<N) == ((robot.y%(2*N))<N)) && (putmarker!(robot))
+        move!(robot,side)
     end
 end
-function moveneed!(robot,side,n)
-    for _ in 1:(n)
-        (!isborder(robot,side))&&(move!(robot,side))
-    end
-end
-function markboard!(robot,n)
+function border!(robot,N)
     side=Ost
-    x=distance!(robot,Ost)
-    flag=false
-    while (distance!(robot,Nord)>=(n-1))
-        do_upora!(robot,inverse(side))
-        (flag && (((x+1)%(2*n))>0)) && move!(robot,side,((x+1)%(2*n)))
-        flag && move!(robot,side,n-1)
-        #flag && moveneed!(robot,side,n-1)
-        drawsquaredline!(robot,side,n)
+    while (!isborder(robot,Nord))
+        passline!(robot,side,N)
+        (((robot.x%(2*N))<N) == ((robot.y%(2*N))<N)) && (putmarker!(robot))
+        move!(robot,Nord)
         side=inverse(side)
-        flag=!flag
-        moveneed!(robot,Nord,n)
     end
+    passline!(robot,side,N)
+    (((robot.x%(2*N))<N) == ((robot.y%(2*N))<N)) && (putmarker!(robot))
 end
-function chessquare!(robot,n)
+function main(robot,N)
+    robot=CoordRobot(robot,0,0)
     y=do_upora!(robot,Sud)
     x=do_upora!(robot,West)
-    markboard!(robot,n)
+    robot.x=0
+    robot.y=0
+    border!(robot,N)
     do_upora!(robot,Sud)
     do_upora!(robot,West)
     move!(robot,Nord,y)
